2da204ee04e63486803161ce401b55ee5579c9b6
guopengzhang <guopeng.zhang2010@gmail.com>
2014-09-20 03:22:35 +0800

    add more general dao interface

--- project-core/src/main/java/net/dumbee/project/core/dao/IBaseDAO.java
+++ project-core/src/main/java/net/dumbee/project/core/dao/IBaseDAO.java
@@ -1,11 +1,22 @@
 package net.dumbee.project.core.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 
 public interface IBaseDAO <T, ID extends Serializable> {
+	
 	T get(ID id);
 	
 	ID save(T entity);
 	
+	void delete(T entity);
+	
+	void saveOrUpdate(T entity);
+	
+	void update(T entity);
+	
+	List<T> findByExample(T entity);
+	
+	List<T> findByExample(T entity, int firstResult, int maxResult);
 }
--- project-service/src/main/java/net/dumbee/project/core/dao/impl/BaseDAO.java
+++ project-service/src/main/java/net/dumbee/project/core/dao/impl/BaseDAO.java
@@ -2,16 +2,17 @@ package net.dumbee.project.core.dao.impl;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
 import net.dumbee.project.core.dao.IBaseDAO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 
-public abstract class BaseDAO<T, ID extends Serializable> implements IBaseDAO<T, ID>{
+public class BaseDAO<T, ID extends Serializable> implements IBaseDAO<T, ID>{
 	
 	@Autowired
-	protected HibernateTemplate hibernateTemplate;
+	protected HibernateTemplate hbtpl;
 	protected Class<T> entityClass;
 	
 	@SuppressWarnings("unchecked")
@@ -21,12 +22,39 @@ public abstract class BaseDAO<T, ID extends Serializable> implements IBaseDAO<T,
 
 	@Override
 	public T get(Serializable id) {
-		return hibernateTemplate.get(entityClass, id);
+		return hbtpl.get(entityClass, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public ID save(T entity) {
+		return (ID)hbtpl.save(entity);
 	}
 
 	@Override
-	public ID save(T entity) {
-		return (ID)hibernateTemplate.save(entity);
+	public List<T> findByExample(T entity) {
+		return hbtpl.findByExample(entity);
 	}
+
+	@Override
+	public void delete(T entity) {
+		hbtpl.delete(entity);
+	}
+
+	@Override
+	public void saveOrUpdate(T entity) {
+		hbtpl.saveOrUpdate(entity);
+	}
+
+	@Override
+	public void update(T entity) {
+		hbtpl.update(entity);
+	}
+	
+	@Override
+	public List<T> findByExample(T entity, int firstResult, int maxResult){
+		return hbtpl.findByExample(entity, firstResult, maxResult);
+	}
+	
 
 }
